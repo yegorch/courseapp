af8057e8186dd3732331c8a62be2d56e0bc100f6
yegorch <yegor.rb@gmail.com>
2021-08-22 17:35:08 +0600

    add gem for confirmable email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable
 
   def to_s
     email 
