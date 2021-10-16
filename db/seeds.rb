216aae9d3ccff1b555ee304f040a3da4c36bcd3a
yegorch <yegor.rb@gmail.com>
2021-10-16 12:51:15 +0400

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create!(email: "admin2@example.com", password: "admin2@example.com", password_confirmation: "admin@example.com", confirmed_at: Time.now)
 
-30.times do
+40.times do
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::GameOfThrones.quote
